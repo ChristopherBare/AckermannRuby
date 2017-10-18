2bc71c79fd575f58a0c5668e1fe2b9adf7cb2d9a
Christopher Bare <christopherbare@outlook.com>
2017-10-18 12:04:53 -0400

    Add files via upload
    INIT COMMIT

---
+++ Ackermann.rb
@@ -0,0 +1,21 @@
+class Ackermann
+  def self.ackermann(m, n)
+    if m == 0
+      2 * n
+    elsif m.to_i >= 1 && n.to_i == 0
+      0
+    elsif m.to_i >= 1 && n.to_i == 1
+      2
+    else
+      return ackermann(m.to_i - 1, ackermann(m.to_i, n.to_i - 1))
+    end
+end
+
+  puts 'what is the value for m'
+  x = gets
+  puts 'what is the value for n'
+  y = gets
+
+  puts 'The result is:'
+  puts ackermann(x,y)
+end
